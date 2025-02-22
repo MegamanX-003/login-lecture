a948eb39b59248c27951e05ca05a0a340b5fd89b
Brian <megamanx021@gmail.com>
2025-02-23 01:34:52 +0900

    update

--- app/app.js
+++ app/app.js
@@ -21,6 +21,9 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+// 절대경로 설정
+app.use(express.static(`${__dirname}/src/public`));
+
 app.use("/", home);  // use -> 미들 웨어를 등록하는 메서드.
 
 module.exports = app;
