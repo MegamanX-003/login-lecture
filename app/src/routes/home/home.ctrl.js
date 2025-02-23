18cf5c1f9e2b9b0915988c2188926c1bc8c00788
Brian <megamanx021@gmail.com>
2025-02-23 18:14:33 +0900

    User Model 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     home: (req, res) => {
@@ -8,31 +8,16 @@ const output = {
     },
     login: (req, res) => {
         res.render("home/login");
-    },
+    }, 
 };
 
 //--------------------------------------------------------------
 
 const process = {
     login: (req, res) => {
-        const id = req.body.id,
-          psword = req.body.psword;
-
-        // console.log(UserStorage.getUsers("id", "psword", "name"));
-        const users = UserStorage.getUsers("id", "psword");
-
-        const response = {};
-        //console.log(id, psword);
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if (users.psword[idx] === psword ) {
-                response.success = true ;
-                return res.json(response);
-            }
-        }
-
-        response.success = false ;
-        response.msg = "로그인에 실패하였습니다.";
+        const users = new User(req.body);
+        const response = users.login();
+        console.log(response);
         return res.json(response);
     },
 };
