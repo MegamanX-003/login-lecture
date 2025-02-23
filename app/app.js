88970d856e3946821c009f5b717c085ad62eea09
Brian <megamanx021@gmail.com>
2025-02-23 12:49:49 +0900

    백엔드에서 로그인 구현

--- app/app.js
+++ app/app.js
@@ -1,17 +1,18 @@
 
 // https://www.youtube.com/playlist?list=PLSK4WsJ8JS4cQ-niGNum4bkK_THHOizTs
 // 저자 : 우리밋
-// 6편 볼차례~
 // https://www.youtube.com/watch?v=AZtyyeCwNoc&list=PLSK4WsJ8JS4cQ-niGNum4bkK_THHOizTs&index=5
 // Express 활용
 // https://www.youtube.com/watch?v=mL84PLyKss0&list=PLSK4WsJ8JS4cQ-niGNum4bkK_THHOizTs&index=7
 // 1:57초
+// 17강 볼차례
 
 
 "use strict";
 
 // 모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 //라우팅
@@ -23,6 +24,10 @@ app.set("view engine", "ejs");
 
 // 절대경로 설정
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+
+// URL을 통해 전달되는 데이터에 한글, 공백 등과 같으 문자가 포함할 경우 인식 문제 해결
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use("/", home);  // use -> 미들 웨어를 등록하는 메서드.
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,15 +1,45 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/index");
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },
+    login: (req, res) => {
+        res.render("home/login");
+    },
 };
 
-const login = (req, res) => {
-    res.render("home/login");
+//--------------------------------------------------------------
+const users = {
+    id: ["brian"],
+    psword: ["1234"],
+};
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+          psword = req.body.psword;
+
+        //console.log(id, psword);
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword ) {
+                console.log("id pw 일치");
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "Login Fail",
+        });
+    },
 };
 
 // key : value 개념
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 };
